9165c3f9d13f19c4790bdcd5f1bf1d0c330d49d4
Yasin Uçar <yasinucar@protonmail.com>
2022-11-27 21:51:13 +0300

    anim

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,18 +9,18 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float speed;
     [SerializeField] float jumpSpeed;
 
-    
+
     Rigidbody2D rig2d;
-   
+
 
     CapsuleCollider2D myFeedCollider;
-   
+
     private void Start()
     {
         rig2d = GetComponent<Rigidbody2D>();
         myFeedCollider = GetComponent<CapsuleCollider2D>();
         anim = gameObject.GetComponent<Animator>();
-     
+
     }
 
     void Update()
@@ -28,17 +28,14 @@ public class PlayerController : MonoBehaviour
         Run();
         FlipSprite();
         Jump();
-       
+
     }
-  
+
     void Run()
     {
-    
-        
-         // rig2d.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rig2d.velocity.y);
 
-            rig2d.velocity=Vector2.right*speed*Time.deltaTime;
-           
+        rig2d.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rig2d.velocity.y);
+
     }
     void FlipSprite()
     {
@@ -48,11 +45,16 @@ public class PlayerController : MonoBehaviour
             transform.localScale = new Vector2(Mathf.Sign(rig2d.velocity.x), 1);
             anim.SetTrigger("run");
         }
+        else
+        {
+            anim.SetTrigger("idle");
+        }
     }
     void Jump()
     {
         if (!myFeedCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
         {
+            anim.SetTrigger("idle2");
             return;
         }
         if (Input.GetButtonDown("Jump"))
@@ -60,6 +62,6 @@ public class PlayerController : MonoBehaviour
             rig2d.velocity = new Vector2(rig2d.velocity.x, jumpSpeed);
             anim.SetTrigger("jump");
         }
-       
+
     }
 }
