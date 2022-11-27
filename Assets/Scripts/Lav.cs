c12fb682a0fc9a268443b584d44078b28ca1dd2a
Cebrail Akyüz <103183740+Cebrail000001F@users.noreply.github.com>
2022-11-28 00:44:46 +0300

    +

---
+++ Assets/Scripts/Lav.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lav : MonoBehaviour
+{
+
+    Rigidbody2D LavRb;
+    [SerializeField] private float speed;
+    void Start()
+    {
+        LavRb = gameObject.GetComponent<Rigidbody2D>();
+        StartCoroutine(bekle());
+
+    }
+
+
+    void Update()
+    {
+        LavRb.velocity = Vector2.up * speed * Time.deltaTime;
+        
+    }
+    IEnumerator bekle()
+    {
+       
+    
+        for (int i = 0; i < 100; i++)
+        {
+           
+            yield return new WaitForSeconds(2f);
+            speed *= 1.02f;
+        }
+    }
+
+}
+
+
+
